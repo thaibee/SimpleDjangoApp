bedecf75f214834db752760c2b09ba166f776c90
magadanthai <doctor.madbee@gmail.com>
2022-01-01 12:38:24 +0700

    Init after 17

---
+++ news/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+
+class News(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Наименование')
+    content = models.TextField(blank=True, verbose_name='Контент')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d', verbose_name='Фото', blank=True)
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'новость'
+        verbose_name_plural = 'новости'
+        ordering = ['-created_at']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Категория')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ['title']
---
+++ news/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from news.models import News, Category
+
+
+def index(request):
+    news = News.objects.all()
+    categories = Category.objects.all()
+    context = {
+        'news': news,
+        'title': 'Cписок новостей',
+        'categories': categories
+    }
+    return render(request, 'news/index.html', context)
+
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    category = Category.objects.get(pk=category_id)
+    categories = Category.objects.all()
+    context = {
+        'news': news,
+        'title': category,
+        'categories': categories
+    }
+    return render(request, 'news/category.html', context)
