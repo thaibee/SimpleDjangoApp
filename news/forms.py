b1908695b400581ade38829898821e80fd2cee89
magadanthai <doctor.madbee@gmail.com>
2022-01-03 11:52:01 +0700

    24 - Три варианта формы(стандартная, вручную, циклом)

--- news/forms.py
+++ news/forms.py
@@ -3,7 +3,9 @@ from .models import Category
 
 
 class NewsForm(forms.Form):
-    title = forms.CharField(max_length=150)
-    content = forms.CharField()
-    is_published = forms.BooleanField()
-    category = forms.ModelChoiceField(queryset=Category.objects.all())
+    title = forms.CharField(max_length=150, label='Название', widget=forms.TextInput(attrs={"class": "form-control"}))
+    content = forms.CharField(label='Текст', widget=forms.Textarea(attrs={"class": "form-control", "rows": "5"}))
+    is_published = forms.BooleanField(label='Опубликовать?', initial=True,
+                                      widget=forms.CheckboxInput(attrs={"class": "form-check-input"}))
+    category = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категория', empty_label='',
+                                      widget=forms.Select(attrs={"class": "form-select"}))
